83c25efa1a546f55ee46820e836d3a8be228b715
jpcuentas123 <jpcuentas123@gmail.com>
2020-02-06 08:19:29 -0500

    Use State React Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ClassCounter from './Components/ClassCounter';
+import UseStateCouter from './Components/UseStateCouter';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter/>
+      {/* <ClassCounter/> */}
+      <UseStateCouter/>
     </div>
   );
 }
